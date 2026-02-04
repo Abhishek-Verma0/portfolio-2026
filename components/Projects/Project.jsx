5ef59c88de9dcd2e8a9c30460bd2dc282e31a8df
Abhishek-Verma0 <neeleshabhishek@gmail.com>
2026-02-04 14:29:32 +0530

    working on about section

--- components/Projects/Project.jsx
+++ components/Projects/Project.jsx
@@ -1,9 +1,37 @@
 "use client";
 import React from "react";
 
+import "./Project.css"
 
-import FloatingLines from '../FloatingLines';
-import TiltedCard from '../TiltedCard';
+import FloatingLines from "../FloatingLines";
+import TiltedCard from "../TiltedCard";
+
+
+const project = [
+  {
+    name: "Secure-Creds",
+    github: "",
+    img: "https://ik.imagekit.io/Mr01Wick/Secure-creds.png?updatedAt=1770091042475",
+    altText: "secure-creds",
+    captionText:"Secure-Creds",
+  },
+  {
+    name: "Artsy AI",
+    github: "",
+    img: "https://ik.imagekit.io/Mr01Wick/Artsy.png?updatedAt=1770091043312",
+  },
+  {
+    name: "Snake Game(C)",
+    link: "",
+    img: "../../public/images/Snake.png",
+  },
+];
+
+
+const mp = project.map((obj, idx) => {
+  
+})
+
 const Project = () => {
   return (
     <div className="page">
@@ -21,27 +49,103 @@ const Project = () => {
         />
       </div>
 
-          <div>
-              <a href="https://google.com" target="_blank">
-                  
-        <TiltedCard
-          imageSrc="https://i.scdn.co/image/ab67616d0000b273d9985092cd88bffd97653b58"
-          altText="Kendrick Lamar"
-          captionText="Kendrick Lamar"
-          containerHeight="300px"
-          containerWidth="300px"
-          imageHeight="300px"
-          imageWidth="300px"
-          rotateAmplitude={12}
-          scaleOnHover={1.05}
-          showMobileWarning={false}
-          showTooltip
-          displayOverlayContent
-          overlayContent={
-              <p className="tilted-card-demo-text">Kendrick Lamar</p>
-            }
+      <div className="all-container">
+        <div className="single-project">
+          <a href="https://github.com/Abhishek-Verma0/Artsy-AI" target="_blank">
+            <TiltedCard
+              imageSrc="https://ik.imagekit.io/Mr01Wick/Artsy.png?updatedAt=1770091043312"
+              altText="Artsy AI"
+              captionText="Artsy"
+              containerHeight="300px"
+              containerWidth="500px"
+              imageHeight="300px"
+              imageWidth="500px"
+              rotateAmplitude={12}
+              scaleOnHover={1.05}
+              showMobileWarning={false}
+              showTooltip
+              displayOverlayContent
+              overlayContent={<p className="tilted-card-demo-text">Artsy AI</p>}
             />
-            </a>
+          </a>
+        </div>
+        {/* project 2 */}
+        <div className="single-project">
+          <a
+            href="https://github.com/Abhishek-Verma0/PETV83L-Abhishek-Verma-Secure-Credential-Manager-With-Key-Encryption"
+            target="_blank"
+          >
+            <TiltedCard
+              imageSrc="https://ik.imagekit.io/Mr01Wick/Secure-creds.png?updatedAt=1770091043312"
+              altText="Secure-Creds"
+              captionText="Secure-Creds"
+              containerHeight="300px"
+              containerWidth="500px"
+              imageHeight="300px"
+              imageWidth="500px"
+              rotateAmplitude={12}
+              scaleOnHover={1.05}
+              showMobileWarning={false}
+              showTooltip
+              displayOverlayContent
+              overlayContent={
+                <p className="tilted-card-demo-text">Secure-Creds</p>
+              }
+            />
+          </a>
+        </div>
+
+        {/* 3rd -project */}
+        <div className="single-project">
+          <a
+            href="https://github.com/Abhishek-Verma0/Snake-Game"
+            target="_blank"
+          >
+            <TiltedCard
+              imageSrc="https://ik.imagekit.io/Mr01Wick/classic-snake.jpg"
+              altText="Classic-Snake"
+              captionText="Classic-Snake"
+              containerHeight="300px"
+              containerWidth="500px"
+              imageHeight="300px"
+              imageWidth="500px"
+              rotateAmplitude={12}
+              scaleOnHover={1.05}
+              showMobileWarning={false}
+              showTooltip
+              displayOverlayContent
+              overlayContent={
+                <p className="tilted-card-demo-text">Classic Snake</p>
+              }
+            />
+          </a>
+        </div>
+
+        {/* project 4 */}
+        <div className="single-project">
+          <a
+            href="github.com/Abhishek-Verma0/Fitness-Streak-Tracker"
+            target="_blank"
+          >
+            <TiltedCard
+              imageSrc="https://ik.imagekit.io/Mr01Wick/streak.webp"
+              altText="Fitness-streak"
+              captionText="Fitness-Streak"
+              containerHeight="300px"
+              containerWidth="500px"
+              imageHeight="300px"
+              imageWidth="500px"
+              rotateAmplitude={12}
+              scaleOnHover={1.05}
+              showMobileWarning={false}
+              showTooltip
+              displayOverlayContent
+              overlayContent={
+                <p className="tilted-card-demo-text">Fitness-Streak</p>
+              }
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
