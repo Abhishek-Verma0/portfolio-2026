88db9dee69b565db364cba98432d80977f28323b
Abhishek Verma <neeleshabhishek@gmail.com>
2026-02-16 05:38:48 +0530

    css resposice about

--- components/about/about.jsx
+++ components/about/about.jsx
@@ -1,37 +1,34 @@
-"use client"
+"use client";
 
-import "./about.css"
-
-
-
-import ScrambledText from "../ScrambledText"
+import ScrambledText from "../ScrambledText";
 import TextArcEffect from "../text-arc-effect";
-
-
+import "./about.css";
 
 const About = () => {
   return (
-    <section className="page-about" id="about">
+    <section className="about-section" id="about">
+      <div className="about-container">
+        <div className="about-arc">
+          <TextArcEffect />
+        </div>
 
-
-   <TextArcEffect />
-    
-      <p>
-        <ScrambledText
-  className="scrambled-text-demo"
-  radius={100}
-  duration={1.2}
-  speed={0.5}
-  scrambleChars=".:"
-        >
-          
-I'm a curious developer with keen interest in Web Development and Machine Learning. As a good learner, I enjoy exploring new technologies, building practical projects, and continuously improving my skills through hands-on experience.
-</ScrambledText>
-   </p>
-
-
+        <div className="about-text">
+          <ScrambledText
+            className="scrambled-text-demo"
+            radius={100}
+            duration={1.2}
+            speed={0.5}
+            scrambleChars=".:"
+          >
+            I'm a curious developer with keen interest in Web Development and
+            Machine Learning. As a good learner, I enjoy exploring new
+            technologies, building practical projects, and continuously
+            improving my skills through hands-on experience.
+          </ScrambledText>
+        </div>
+      </div>
     </section>
   );
-}
+};
 
-export default About
+export default About;
